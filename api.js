432347912e26492742d963630cc6a5cb922bdd56
Jev Forsberg <jevf@turbinelabs.com>
2022-11-30 12:04:54 -0700

    master/ add uuid

--- api.js
+++ api.js
@@ -1,9 +1,25 @@
 const client = require("./connection.js");
 const express = require("express");
-const app = express();
-
-app.listen(3300, () => {
-  console.log("Sever is now listening at port 3300");
-});
+const bodyParser = require("body-parser");
 
 client.connect();
+
+const PORT = 3000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Sever is now listening at port ${PORT}`);
+});
+
+const getAllRows = (res, tableName) => {
+  client.query(`Select * from ${tableName}`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+    } else throw err;
+  });
+  client.end;
+};
+
+app.get("/users", (_req, res) => getAllRows(res, "users"));
