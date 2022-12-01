9c57495c14e21623228509b9f3e00c02ad2df255
Jev Forsberg <jevf@turbinelabs.com>
2022-12-01 09:58:54 -0700

    master/ add dbmanip file

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
 const client = require("./connection.js");
 const express = require("express");
 const bodyParser = require("body-parser");
-const { v4: uuidv4 } = require("uuid");
-
-// const data = CSVToJSON("./test.csv").then((data) => data);
+const {
+  insertRow,
+  getAllRows,
+  getSingleRow,
+} = require("./databaseManipulation");
 
 client.connect();
 
@@ -16,44 +18,44 @@ app.listen(PORT, () => {
   console.log(`Sever is now listening at port ${PORT}`);
 });
 
-const insertRow = (body, tableName, res) => {
-  const id = uuidv4();
-  const keys = Object.keys(body);
-  const itemName = tableName.slice(0, -1);
-  const successMessage = `Insertion was successful of new ${itemName} of ID: ${id}`;
-  const stringifiedKeys = keys.join(", ");
-  const stringifiedValues = keys.map((key) => body[key]).join("', '");
-  const insertQuery = `INSERT INTO ${tableName}(id, ${stringifiedKeys}) values('${id}', '${stringifiedValues}')`;
+// const insertRow = (body, tableName, res) => {
+//   const id = uuidv4();
+//   const keys = Object.keys(body);
+//   const itemName = tableName.slice(0, -1);
+//   const successMessage = `Insertion was successful of new ${itemName} of ID: ${id}`;
+//   const stringifiedKeys = keys.join(", ");
+//   const stringifiedValues = keys.map((key) => body[key]).join("', '");
+//   const insertQuery = `INSERT INTO ${tableName}(id, ${stringifiedKeys}) values('${id}', '${stringifiedValues}')`;
 
-  client.query(insertQuery, (err, _result) => {
-    if (!err) {
-      if (res) {
-        res.send(successMessage);
-      } else {
-        console.log(successMessage);
-      }
-    } else throw err;
-  });
-  client.end;
-};
+//   client.query(insertQuery, (err, _result) => {
+//     if (!err) {
+//       if (res) {
+//         res.send(successMessage);
+//       } else {
+//         console.log(successMessage);
+//       }
+//     } else throw err;
+//   });
+//   client.end;
+// };
 
-const getAllRows = (res, tableName) => {
-  client.query(`SELECT * FROM ${tableName}`, (err, result) => {
-    if (!err) {
-      res.send(result.rows);
-    } else throw err;
-  });
-  client.end;
-};
+// const getAllRows = (res, tableName) => {
+//   client.query(`SELECT * FROM ${tableName}`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows);
+//     } else throw err;
+//   });
+//   client.end;
+// };
 
-const getSingleRow = (res, tableName, id) => {
-  client.query(`SELECT * FROM ${tableName} WHERE id=${id}`, (err, result) => {
-    if (!err) {
-      res.send(result.rows);
-    } else throw err;
-  });
-  client.end;
-};
+// const getSingleRow = (res, tableName, id) => {
+//   client.query(`SELECT * FROM ${tableName} WHERE id=${id}`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows);
+//     } else throw err;
+//   });
+//   client.end;
+// };
 
 app.get("/users", (_req, res) => getAllRows(res, "users"));
 app.get("/users/:id", (req, res) => {
